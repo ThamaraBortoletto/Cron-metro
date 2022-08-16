0e6419a2f0fd586a8c98f7e7629b282ee87569bb
Thamara <themarabortoletto@hotmail.com>
2022-08-16 15:54:56 -0300

    first commit

---
+++ src/Components/Botao/index.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+class botao extends react.Component {
+    render() {
+        return (
+            <button> Botão </button>
+        )
+    }
+}
+
+
+export default botao;
