ab4d4f8e505b08e003dfb871e36aa60a9fde28cc
Thamara <themarabortoletto@hotmail.com>
2022-09-08 15:01:51 -0300

    Lista

---
+++ src/Components/Lista/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Lista(){
+    return(
+        <aside>
+            <h2>Estudos do Dia</h2>
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>01:30:00</span>
+                </li>
+                <li>
+                    <h3>Java Script</h3>
+                    <span>00:40:00</span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
